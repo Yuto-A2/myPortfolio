26e608df657d404b1a1eb37238aa847964a1e106
Yuto Arimori <n01648493@humber.ca>
2024-08-16 20:46:46 -0400

    updated

--- portfolio/frontend/src/components/AboutList.jsx
+++ portfolio/frontend/src/components/AboutList.jsx
@@ -4,7 +4,7 @@ export default function AboutList() {
 // this rought get the life event for about page from the database
   useEffect(() => {
     const getPortfolios = async () => {
-      let response = await fetch(`${process.env.REACT_APP_API_URL}/api/about`);
+      let response = await fetch("https://my-portfolio-backend-chi.vercel.app/api/work/api/about");
       let data = await response.json();
       console.log(data);
       setPortfolios(data);
