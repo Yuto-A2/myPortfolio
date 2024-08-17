03eeaee0b7a25648797f0ec45c17a15ebf633aa3
Yuto Arimori <n01648493@humber.ca>
2024-08-16 20:41:18 -0400

    update

--- portfolio/frontend/src/components/AboutList.jsx
+++ portfolio/frontend/src/components/AboutList.jsx
@@ -4,7 +4,7 @@ export default function AboutList() {
 // this rought get the life event for about page from the database
   useEffect(() => {
     const getPortfolios = async () => {
-      let response = await fetch(`https://${process.env.REACT_APP_API_URL}/api/about`);
+      let response = await fetch(`${process.env.REACT_APP_API_URL}/api/about`);
       let data = await response.json();
       console.log(data);
       setPortfolios(data);
