7a0cac1f995ad2f22db35a8e090a32d398c2ed6f
Yuto Arimori <n01648493@humber.ca>
2024-08-16 20:32:15 -0400

    updated

--- portfolio/frontend/src/components/Contact.jsx
+++ portfolio/frontend/src/components/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
       userEmail: e.target.userEmail.value,
       userMsg: e.target.userMsg.value
     };
-    await fetch(`http://${process.env.REACT_APP_API_URL}/api/email?userName=${contactForm.userName}&userEmail=${contactForm.userEmail}&userMsg=${contactForm.userMsg}`);
+    await fetch(`https://${process.env.REACT_APP_API_URL}/api/email?userName=${contactForm.userName}&userEmail=${contactForm.userEmail}&userMsg=${contactForm.userMsg}`);
   }
     return(
         <>
