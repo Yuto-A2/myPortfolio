6ab3e2d6c9683832797757c0ebfe93fb4925f275
Yuto Arimori <n01648493@humber.ca>
2025-02-02 16:36:35 -0500

    changed

--- portfolio/frontend/src/components/Works.jsx
+++ portfolio/frontend/src/components/Works.jsx
@@ -9,7 +9,7 @@ export default function Works() {
     const getWorks = async () => {
       let response = await fetch("https://my-portfolio-backend-chi.vercel.app/api/work");
       let data = await response.json();
-      console.log(data); 
+      console.log(data);
       setWorks(data);
     };
     getWorks();
@@ -24,14 +24,14 @@ export default function Works() {
 
     // out of index
     if (newIndex < 0) {
-      newIndex = works.length - 1; 
+      newIndex = works.length - 1;
     } else if (newIndex >= works.length) {
-      newIndex = 0; 
+      newIndex = 0;
     }
 
     setCurrentIndex(newIndex); //  new index
-    const slideWidth = container.getBoundingClientRect().width; 
-    container.style.transform = `translateX(${-slideWidth * newIndex}px)`; 
+    const slideWidth = container.getBoundingClientRect().width;
+    container.style.transform = `translateX(${-slideWidth * newIndex}px)`;
   };
 
   return (
@@ -43,7 +43,7 @@ export default function Works() {
             className="worksBox"
             ref={worksContainerRef}
             style={{
-              transform: `translateX(${-100 * currentIndex}%)`, 
+              transform: `translateX(${-100 * currentIndex}%)`,
             }}
           >
             {works.map((work) => (
@@ -64,11 +64,28 @@ export default function Works() {
         {works.map((_, index) => (
           <button
             key={index}
-            className={index === currentIndex ? "current" : ""} 
-            onClick={() => setCurrentIndex(index)} 
+            className={index === currentIndex ? "current" : ""}
+            onClick={() => setCurrentIndex(index)}
           ></button>
         ))}
       </nav>
+
+      {/* modal */}
+      {/* <div className="modalContainer">
+        <h3>使用言語</h3>
+        <p>HTML, CSS, JavaScript</p>
+        <p><a href="#">Go to GitHub page.</a></p>
+        <p><a href="#">Go to the web page.</a></p>
+        <h3>About the work</h3>
+        <p>設計からデザインまで一からすべて自分で行いました。</p>
+        <p>日本語の学習、特にJLPTという日本語能力試験の練習に特化したアプリです。</p>
+        <ul>
+          <li>"単語・文法・リスニングを効率よく学習できる機能を搭載"</li>
+          <li>"進捗管理機能で学習のモチベーションを維持"</li>
+          <li>"モバイル対応で、どこでも手軽に学習可能"</li>
+        </ul>
+        <img src="#" alt="" />
+      </div> */}
     </>
   );
 }
