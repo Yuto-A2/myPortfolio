ce8cff785ab0f7c4e4202cbfef0317bb60ab9ea4
Yuto Arimori <n01648493@humber.ca>
2025-03-27 18:02:03 -0400

    adjuct to dark mode.

--- portfolio/frontend/src/components/AboutComponent.jsx
+++ portfolio/frontend/src/components/AboutComponent.jsx
@@ -7,11 +7,11 @@ export default function AboutComponent(){
           <p><img className="profileImg" src="/img/yutoA2.jpg" alt="profile photo" /></p>
         </div>
         <div className="aboutExplain">
-          <p>I take Web Development Program.</p>
+          {/* <p>I take Web Development Program.</p>
           <p>I learn coding and web design.</p>
           <p>My goal is to help you make a website you want.</p>
           <p>I know not only how to code and design also but SEO and web marketing.</p>
-          <p>Please contact me for more information.</p>
+          <p>Please contact me for more information.</p> */}
         </div>{/* aboutElain */}
       </div>{/* about */}
       </>
