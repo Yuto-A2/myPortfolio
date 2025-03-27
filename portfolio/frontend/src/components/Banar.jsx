0974ba765ba0380e71ffb78f82e7de372083b406
Yuto Arimori <n01648493@humber.ca>
2025-03-27 17:36:57 -0400

    Added animation on Home.css

--- portfolio/frontend/src/components/Banar.jsx
+++ portfolio/frontend/src/components/Banar.jsx
@@ -1,7 +1,39 @@
 export default function Banar() {
-    return (
-        <>
-          <p><img id="banar" src="/img/yutos.png" alt="" /></p>
-        </>
-    )
+  return (
+    <>
+      {/* <p><img id="banar" src="/img/yutos.png" alt="" /></p> */}
+      <div className="banar">
+        <div className="banarBox">
+          <div className="banarTitle">
+            <p className="welcome2">
+              <span className="y">Y</span>
+              <span className="u">u</span>
+              <span className="t">t</span>
+              <span className="o">o</span>
+              <span className="apos">'</span>
+              <span className="s">s</span> <span className="p">p</span>
+              <span className="o2">o</span>
+              <span className="r">r</span>
+              <span className="t2">t</span>
+              <span className="f">f</span>
+              <span className="o3">o</span>
+              <span className="l">l</span>
+              <span className="i">i</span>
+              <span className="o4">o</span>
+            </p>
+          </div>
+          <div className="banarExplanation">
+            <p>My name is Yuto Arimori, a full-stack developer based in Toronto, Canada.</p>
+            <p>I am a highly motivated professional with strong communication skills, honed through my teaching experience.</p>
+            <p>With expertise in both front-end and back-end development, I excel at conducting in-depth research to efficiently solve complex challenges.</p>
+            <p>I possess strong organizational and problem-solving abilities, enabling me to manage tasks effectively and deliver high-quality results.</p>
+          </div>
+        </div>
+        <div className="banarContainer">
+          <img src="/img/pcImg.jpg" alt="" className="banarImg" />
+        </div>
+
+      </div>
+    </>
+  )
 }
